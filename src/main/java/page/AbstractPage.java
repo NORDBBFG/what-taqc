3861bfea6a227bce45721eebb942175994555857
MashaLar <mashalar01@gmail.com>
2021-04-21 19:13:56 +0300

    Edited AbstractPage

--- src/main/java/page/AbstractPage.java
+++ src/main/java/page/AbstractPage.java
@@ -1,6 +1,16 @@
 package page;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+
 public abstract class AbstractPage {
 
-    // TODO driver dependency
+   private WebDriver driver;
+
+   public AbstractPage(WebDriver driver){
+       this.driver = driver;
+       PageFactory.initElements(this.driver, this);
+   }
+
 }
