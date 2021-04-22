141b5a11962dbde9ca50de07f9c133a58c510ea2
NORDBBFG <nbasisty911@gmail.com>
2021-04-22 20:15:57 +0300

    Add some methods in AbstractPage

--- src/main/java/page/AbstractPage.java
+++ src/main/java/page/AbstractPage.java
@@ -1,6 +1,7 @@
 package page;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 
 
@@ -12,4 +13,14 @@ public abstract class AbstractPage {
         this.driver = driver;
         PageFactory.initElements(this.driver, this);
     }
+
+    protected void fillField(WebElement element, String inputText) {
+        element.clear();
+        element.sendKeys(inputText);
+    }
+
+    protected  String getText(WebElement element){
+        return element.getText();
+    }
+
 }
