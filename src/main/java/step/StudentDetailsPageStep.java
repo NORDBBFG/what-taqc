3772e00c3308555b176404b89af8897817e135f6
vbahno <vbahno@softserveinc.com>
2021-05-04 11:19:10 +0300

    Created verify for page Student Details

--- src/main/java/step/StudentDetailsPageStep.java
+++ src/main/java/step/StudentDetailsPageStep.java
@@ -1,12 +1,13 @@
 package step;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import page.StudentDetailsPage;
 
 public class StudentDetailsPageStep {
 
-    private StudentDetailsPage studentDetailsPage;
+    StudentDetailsPage studentDetailsPage;
 
     public StudentDetailsPageStep(WebDriver driver){
         studentDetailsPage = new StudentDetailsPage(driver);
@@ -32,13 +33,57 @@ public class StudentDetailsPageStep {
         return this;
     }
 
-    public StudentDetailsPageStep verifyEnabledGroups(boolean countGroup){
-        Assert.assertEquals(studentDetailsPage.listGroupsIsEmpty(), countGroup);
+    // TODO return GroupDetailsPage
+    public void clickOnLabelOfGroup(String labelOfGroup) {
+        for (WebElement elementOfGroup: studentDetailsPage.getListOfGroups()) {
+            if(elementOfGroup.getText().equals(labelOfGroup)){
+                elementOfGroup.click();
+                break;
+            }
+        }
+    }
+
+    public StudentDetailsPageStep verifyLabelOfGroupsExists(String expected){
+        String actual = null;
+        for (String labelOfGroups: studentDetailsPage.getListLabelsOfGroups()) {
+            if(labelOfGroups.equals(expected)) {
+                actual = labelOfGroups;
+                break;
+            }
+        }
+        Assert.assertEquals(actual, expected);
         return this;
     }
 
-    public StudentDetailsPageStep verifyEnabledLessons(boolean countLesson){
-        Assert.assertEquals(studentDetailsPage.listLessonsIsEmpty(), countLesson);
+    public StudentDetailsPageStep verifyListOfGroupsIsDisplayed() {
+        Assert.assertTrue(studentDetailsPage.getQuantityGroup() > 0, "List of groups is displayed");
+        return this;
+    }
+
+    // TODO return LessonDetailsPage
+    public void clickOnLabelOfLesson(String labelOfLesson) {
+        for (WebElement elementOfLesson: studentDetailsPage.getListOfLessons()) {
+            if(elementOfLesson.getText().equals(labelOfLesson)){
+                elementOfLesson.click();
+                break;
+            }
+        }
+    }
+
+    public StudentDetailsPageStep verifyLabelOfLessonExists(String expected){
+        String actual = null;
+        for (String labelOfLessons: studentDetailsPage.getListLabelsOfLessons()) {
+            if(labelOfLessons.equals(expected)) {
+                actual = labelOfLessons;
+                break;
+            }
+        }
+        Assert.assertEquals(actual, expected);
+        return this;
+    }
+
+    public StudentDetailsPageStep verifyListOfLessonsIsDisplayed() {
+        Assert.assertTrue(studentDetailsPage.getQuantityLessons() > 0, "List of lessons is displayed");
         return this;
     }
 
