3a2f06b65e38655c6c1cc83ad2742e0d3e37a1c5
vbahno <vbahno@softserveinc.com>
2021-05-07 18:23:00 +0300

    Added test studentsPageSwitchBetweenTabs_WHAT_123

--- src/main/java/test/StudentDetailsTest.java
+++ src/main/java/test/StudentDetailsTest.java
@@ -4,11 +4,13 @@ import org.openqa.selenium.By;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import page.SignInPage;
+import step.EditStudentDetailsPageStep;
 import step.StudentDetailsPageStep;
 
 public class StudentDetailsTest extends AbstractTest {
 
     StudentDetailsPageStep studentDetailsPageStep;
+    EditStudentDetailsPageStep editStudentDetailsPageStep;
 
     @BeforeMethod
     public void preconditions() {
@@ -21,15 +23,40 @@ public class StudentDetailsTest extends AbstractTest {
         signInPage.clickSignInButton();
 
         studentDetailsPageStep = new StudentDetailsPageStep(driver);
-
-        // click on current student in the list of students
-        driver.findElement(By.xpath("//tr[@data-student-id='5']")).click();
+        editStudentDetailsPageStep = new EditStudentDetailsPageStep(driver);
     }
 
     @Test
-    public void checkListOfLessonsNotDisplayed_tc198(){
+    public void studentDetailsLessonsDisplayed_WHAT_197(){
+
+        //step('1')
+        // click on current student in the list of students
+        driver.findElement(By.xpath("//tr[@data-student-id='5']")).click();
 
         studentDetailsPageStep
                 .verifyListOfLessonsIsDisplayed();
+    }
+
+    @Test
+    public void studentsPageSwitchBetweenTabs_WHAT_123(){
+
+        //step('1')
+        // click "Edit" on a Student
+        driver.findElement(By.xpath("(//td/*[name()='svg'])[1]")).click();
+
+        editStudentDetailsPageStep
+                .verifyLabelEditStudentsDetails(Constants.PageName.STUDENT_EDIT_DETAILS)
+                .verifyFirstName("Голлумм")
+                .verifyLastName("Смеагол")
+                .verifyEmail("ILoveRings@gmail.com")
+                .verifyListOfGroupsIsDisplayed()
+        //step('2')
+                .clickLabelStudentsDetails()
+                .verifyLabelStudentsDetails(Constants.PageName.STUDENT_DETAILS);
+        studentDetailsPageStep
+                .verifyFirstName("Голлумм")
+                .verifyLastName("Смеагол")
+                .verifyEmail("ILoveRings@gmail.com")
+                .verifyListOfGroupsIsDisplayed();
     }
 }
