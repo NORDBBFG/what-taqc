d841c087d9f48f3bdb86882bd55ce367579d070c
MashaLar <mashalar01@gmail.com>
2021-05-09 00:29:04 +0300

    Created BaseStep
    Created added SignInPage to BaseTest preconditions

---
+++ src/main/java/constants/Classes.java
@@ -0,0 +1,33 @@
+package constants;
+
+public class Classes {
+
+    public interface Steps {
+
+        String CHANGE_PASSWORD_PAGE_STEPS = "step.changepassword.ChangePasswordPageSteps",
+
+                ADD_COURSE_STEP = "step.courses.AddCourseStep",
+                COURSE_DETAILS_STEP = "step.courses.CourseDetailsStep",
+                COURSES_STEP = "step.courses.CoursesStep",
+                EDIT_COURSE_DETAILS_STEP = "step.courses.EditCourseDetailsStep",
+
+                LESSON_DETAILS_PAGE_STEP = "step.lesson.LessonDetailsPageStep",
+                LESSON_EDIT_PAGE_STEP = "step.lesson.LessonEditPageStep",
+                LIST_OF_LESSON_PAGE_STEP = "step.lesson.ListOfLessonPageStep",
+
+                MY_PROFILE_PAGE_STEP = "step.myprofile.MyProfilePageStep",
+
+                REGISTRATION_PAGE_STEP = "step.registration.RegistrationPageStep",
+                SIGN_IN_PAGE_STEP = "step.signin.SignInPageStep",
+
+                STUDENT_DETAILS_PAGE_STEP = "step.student.EditStudentDetailsPageStep",
+                STUDENT_STUDENT_DETAILS_PAGE_STEP = "step.student.StudentDetailsPageStep",
+                LIST_OF_STUDENTS_PAGE_STEP = "step.student.ListOfStudentsPageStep",
+
+                UNASSIGNED_USERS_STEP = "step.unassigned.UnassignedUsersStep",
+
+                CONFIRM_ACTION_WINDOW_STEPS = "step.ConfirmActionWindowSteps";
+
+    }
+
+}
--- src/main/java/step/BaseStep.java
+++ src/main/java/step/BaseStep.java
@@ -1,9 +1,95 @@
 package step;
 
+import constants.Constants;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import page.BasePage;
+import step.courses.CoursesStep;
+import step.lesson.ListOfLessonPageStep;
+import step.myprofile.MyProfilePageStep;
+import step.student.ListOfStudentsPageStep;
+
+import java.lang.reflect.InvocationTargetException;
+
 public abstract class BaseStep {
 
+    public ListOfStudentsPageStep clickStudentSidebar(Class<? extends BasePage> context, WebDriver driver) {
+        try {
+            Thread.sleep(5000); //due to load list
+            BasePage basePage = context.getConstructor(WebDriver.class).newInstance(driver);
+            basePage.clickSidebarItem(Constants.PageName.STUDENT_LIST);
+            return new ListOfStudentsPageStep(driver);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException | InstantiationException e) {
+            return null;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public ListOfLessonPageStep clickLessonSidebar(Class<? extends BasePage> context, WebDriver driver) {
+        try {
+            Thread.sleep(5000); //due to load list
+            BasePage basePage = context.getConstructor(WebDriver.class).newInstance(driver);
+            basePage.clickSidebarItem(Constants.PageName.LESSON_LIST);
+            return new ListOfLessonPageStep(driver);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException | InstantiationException e) {
+            return null;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public CoursesStep clickCoursesSidebar(Class<? extends BasePage> context, WebDriver driver) {
+        try {
+            Thread.sleep(5000); //due to load list
+            BasePage basePage = context.getConstructor(WebDriver.class).newInstance(driver);
+            basePage.clickSidebarItem(Constants.PageName.GROUP_LIST);
+            return new CoursesStep(driver);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException | InstantiationException e) {
+            return null;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 
 
+    public MyProfilePageStep clickMyProfile(Class<? extends BasePage> context, WebDriver driver) {
+        try {
+            BasePage basePage = context.getConstructor(WebDriver.class).newInstance(driver);
+            basePage.clickArrowIcon();
+            basePage.clickDropdownItem(Constants.PageName.MY_PROFILE);
+            return new MyProfilePageStep(driver);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException | InstantiationException e) {
+            return null;
+        }
+    }
 
+
+    public <T extends BaseStep> T verifySidebarItemLessons(Class<? extends BaseStep> stepContext,
+                                             Class<? extends BasePage> pageContext,
+                                             boolean expected, WebDriver driver) {
+        try {
+            BasePage basePage = pageContext.getConstructor(WebDriver.class).newInstance(driver);
+            Assert.assertEquals(basePage.dropdownItemExist(Constants.PageName.LESSON_LIST), expected);
+            return (T) stepContext.newInstance();
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException | InstantiationException e) {
+            return null;
+        }
+    }
+
+    public <T extends BaseStep> T verifySidebarItemStudents(Class<? extends BaseStep> stepContext,
+                                              Class<? extends BasePage> pageContext,
+                                              boolean expected, WebDriver driver) {
+        try {
+            BasePage basePage = pageContext.getConstructor(WebDriver.class).newInstance(driver);
+            Assert.assertEquals(basePage.dropdownItemExist(Constants.PageName.STUDENT_LIST), expected);
+            return (T) stepContext.newInstance();
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException | InstantiationException e) {
+            return null;
+        }
+    }
 
 }
