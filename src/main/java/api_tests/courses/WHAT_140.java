9a904204b3b4c8d002bcd91b16f11344fc057560
vl3xit <pro100kef@gmail.com>
2021-05-21 18:01:49 +0300

    Make WHAT_140 test.

---
+++ src/main/java/api_tests/courses/WHAT_140.java
@@ -0,0 +1,36 @@
+package api_tests.courses;
+
+import api_tests.BaseTest;
+import io.restassured.http.ContentType;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class WHAT_140 extends BaseTest {
+
+    @Test
+    public void disableCourse200Admin(){
+        String courseID = "61";
+        String courseName = ".NET for beginners";
+        String adminToken = getAdminToken();
+
+        // (step'1')
+        given().
+                header("Authorization",adminToken).
+                contentType(ContentType.BINARY).
+                when().delete("https://whatbackend.azurewebsites.net/api/courses/" + courseID).
+                then().assertThat().statusCode(200).
+                and().assertThat().body(hasToString("true")).
+                and().log().body();
+
+        // (step'2')
+        given().
+                header("Authorization",adminToken).
+                when().get("https://whatbackend.azurewebsites.net/api/courses/isActive").
+                then().assertThat().statusCode(200).
+                and().body("[60].name",hasToString(courseName)).
+                and().body("[60].isActive", hasToString("false")).
+                and().log().body();
+    }
+}
