2f1bbf7c5a72a7b99cf936a1c89848d8778373e5
Mr.SkeletonFox <kab.alex.it@gmail.com>
2021-05-22 00:59:27 +0300

    create WHAT_172.java
    This test case verifies that the Administrator user can disable the Mentor by making DELETE request to the REST endpoint.

---
+++ src/main/java/api_tests/mentor/WHAT_172.java
@@ -0,0 +1,25 @@
+package api_tests.mentor;
+
+
+import api_tests.BaseTest;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasToString;
+
+public class WHAT_172 extends BaseTest {
+
+    @Test
+    public void getNotAssigned200Admin(){
+        String id_mentor = "38";
+
+        given().
+                header("Authorization",getAdminToken()).
+                when().delete("https://whatbackend.azurewebsites.net/api/mentors/" + id_mentor).
+                then().assertThat().statusCode(200).
+                and().assertThat().body(hasToString("true")).
+
+
+                and().log().body();
+    }
+}
